ed31f63bf7bf36638e5e4d65153c50ad5b811acb
khosta77 <puwerfulpants@gmail.com>
2024-03-17 00:34:48 +0300

    Задания по первой лекции

---
+++ L01/C31-3.py
@@ -0,0 +1,7 @@
+# Необходимо написать программу, которая будет считывать 
+# со стандартного ввода название города и время и выводить
+# информацию в формате 
+# “Current location is [Location] and time is [Time]”.
+location = input()
+time = input()
+print("Current location is " + location + " and time is " + time)
