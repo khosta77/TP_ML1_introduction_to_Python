f68556c71267d45c7531deadef86219910b15893
khosta77 <puwerfulpants@gmail.com>
2024-04-11 21:07:28 +0300

    Не проходит тесты, в чем ошибка не понятно

---
+++ L06/C314-3.py
@@ -0,0 +1,15 @@
+# Написать функцию parse_time, которая принимает строку в качестве параметра, которая является временем
+# формата “ГГГГ ММ ДД ЧЧ ММ СС” и парсит эту строку в объект datetime.datetime и сдвигает ее на
+# один день вперед. 
+
+# Дополните код ниже, дописав свой код в секции “YOUR CODE HERE”.
+
+import datetime
+
+string_datetime = input()
+
+def parse_time(s):
+    date = datetime.datetime.strptime(s, "%Y %m %d %H %M %S") + datetime.timedelta(days=1)
+    return date.strftime("%Y %m %d %H %M %S")
+
+print(parse_time(string_datetime))
